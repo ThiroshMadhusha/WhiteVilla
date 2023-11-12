47a7c2d07d0c13e4029d1b4111358e80a8d486fb
Thirosh Madhusha <thiroshmadhusha0520@gmail.com>
2023-11-12 17:01:32 +0530

    change delay

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,8 +117,8 @@ themeButton.addEventListener('click', () => {
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 2500,
-    delay: 400,
+    duration: 2000,
+    delay: 200,
     // reset: true
    
 })
